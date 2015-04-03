5308d1cbfd4debf83643169e855102e607f6323c
Shivanand Naik <centrio@gmail.com>
2015-04-03 11:43:44 -0700

    Added/deleted old/new files

--- MoveConstruct/src/main.cpp
+++ MoveConstruct/src/main.cpp
@@ -1,3 +1,9 @@
+#include<iostream>
+#include<memory>
+using namespace std;
+
+
+
 int main()
 {
 
--- bld.sh
+++ bld.sh
@@ -2,7 +2,7 @@
 
 function usage {
   echo "Usage ./bld.sh <ProjectName>"
-  echo "Usage ./bld.sh <ProjectName> clean"
+  echo "Usage ./bld.sh clean"
 }
 
 if [ $# -lt 1 ]; 
@@ -10,33 +10,23 @@ then
     usage
     exit
 fi
-projectName=$1
+export projectName=$1
 if [ "$projectName" == "clean" ] || [ "$projectName" == "Clean" ];
 then
-    usage
-    exit
-fi
-
-if [ ! -d "$projectName" ];
+    echo "Cleaning"
+    cd build/
+    rm -rf * >/dev/null 2>&1
+    cd - >/dev/null 2>&1
+elif [ ! -d "$projectName" ];
 then
     echo "Project does not exists :" $projectName
     exit
-fi
-
 #pName=${PWD##*/}
-sed -i "s/^PROJECT\(.*\)/PROJECT\( $projectName \)/g" $projectName/src/CMakeLists.txt 
-
-if [ $# -eq 1 ] && { [ "$1" == "clean" ] || [ "$1" == "Clean" ]; };
-then
-    echo "Cleaning"
-    cd build/$projectName
-    rm -rf * >/dev/null 2>&1
-    cd - >/dev/null 2>&1
-    rmdir build/$projectName
+#sed -i "s/^PROJECT\(.*\)/PROJECT\( $projectName \)/g" $projectName/src/CMakeLists.txt 
 else
     echo "BUILDING"
     mkdir -p build/$projectName 2>/dev/null 1>&2
-    cd build/$projectName && cmake ../../$projectName/src
+    cd build/$projectName && cmake ../../
     make
     cd -
 fi
